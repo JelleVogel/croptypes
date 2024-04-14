ac21a8a4733a7c6ade7edfeef2407bc314e1ab54
Niek Schattenberg <n.e.schattenberg@student.tudelft.nl>
2024-04-14 22:06:45 +0200

    Tidy up: Removed temporary test images of API handling

--- test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Example for a well-known location (e.g., Times Square, New York)
-test_url = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=40.758896,-73.985130&key=AIzaSyAes1mHa3VTn9T3hMUNJhlnJ_7DS4XT5so"
-response = requests.get(test_url)
-
-if response.status_code == 200:
-    with open('test_image.jpg', 'wb') as file:
-        file.write(response.content)
